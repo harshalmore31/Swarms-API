2948abf2358519031b2edfe0053371d24317fa12
Kye Gomez <kye@swarms.world>
2025-04-18 10:16:29 -0700

    cleanup

--- mcs_api.py
+++ mcs_api.py
@@ -8,7 +8,9 @@ import json
 load_dotenv()
 
 API_KEY = os.getenv("SWARMS_API_KEY")
-BASE_URL = "http://localhost:8080"
+# BASE_URL = "https://swarms-api-285321057562.us-east1.run.app"
+BASE_URL = "https://api.swarms.world"
+
 
 headers = {"x-api-key": API_KEY, "Content-Type": "application/json"}
 
